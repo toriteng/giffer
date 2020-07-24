a94e5f2b50bb5486384dff1942e495e7c091592f
Tori Teng <toriteng@Toris-MacBook-Pro.local>
2020-07-23 23:23:59 -0700

    giffer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import './App.css'
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 function App() {
+  const [text,setText] = useState('')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="searchbar">
+        <TextField label ="Outlined" variant="outlined" color="white"
+        placeholder="Search for a Meme" value={text} onChange={e=> setText(e.target.value)}
+        />
+        <Button variant="contained" color="primary"
+        style={{height:55, marginLeft:8}}
+        disabled={!text}>
+          Search
+        </Button>
+      </div>
     </div>
   );
 }
